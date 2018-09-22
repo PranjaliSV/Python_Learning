6a687f2c1b6a9ece597aff2fc0a75581ad0a6526
PranjaliSV <pranjalivakil@gmail.com>
2018-09-22 21:52:40 +0530

    Added End and Comments

--- PythonFirstProgram.py
+++ PythonFirstProgram.py
@@ -1,7 +1,33 @@
+#----------------------------HelloWorld----------------------------------
+#-----------------------------Function-----------------------------------
+#-----------------------------For-Range----------------------------------
+
 def function1():
     for x in range(-1,8,2):
         print("num", x , sep="-")
         
 print('hello world')
 function1()
+
+
+#------------------------------end---------------------------------------
+
+# This Python program must be run with 
+# Python 3 as it won't work with 2.7. 
+  
+# ends the output with a <space>  
+print("Good Morning")  
+print("Good Day") 
+
+print("Good Morning" , end = ' ')  
+print("Good Day") 
+
+# ends the output with '@' 
+print("Good Morning" , end = '@')  
+print("Folks") 
+
+# ends the output with '@' 
+print("Good Morning" , end = ' Yayayayay ')  
+print("Folks") 
+
 
