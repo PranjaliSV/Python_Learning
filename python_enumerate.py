0b58c8e6d79ce50086ca36d09b097f3c5ccdb446
PranjaliSV <pranjalivakil@gmail.com>
2018-09-22 21:30:37 +0530

    Updating enumerate

--- python_enumerate.py
+++ python_enumerate.py
@@ -12,15 +12,33 @@ print ("obj_l1", obj_l1)
 print ("Return type:", type(obj_l1)) 
 print ("list_enumerate_l1", list(enumerate(l1))) 
 # changing start index to 2 from 0 
-print (list(enumerate(l1,-10)), "\n\n\n")
+print (list(enumerate(l1,-10)))
+# enumerate function in loops 
 
+# printing the tuples in object directly 
+for ele in enumerate(l1): 
+    print ("ele => ", ele) 
+print 
+# changing index and printing separately 
+for count,ele in enumerate(l1,100): 
+    print ("count, ele => ", count,ele) 
+    
+print("\n\n\n")
 print ("s1", s1, "\n") 
 print ("obj_s1", obj_s1)
 print ("Return type:", type(obj_s1)) 
 print ("list_enumerate_s1", list(enumerate(s1))) 
 # changing start index to 2 from 0 
 print (list(enumerate(s1,2)))
+# printing the tuples in object directly 
+for ele in enumerate(s1): 
+    print ("ele => ", ele) 
+print 
+# changing index and printing separately 
+for count,ele in enumerate(s1,100): 
+    print ("count, ele => ", count,ele) 
 
 
   
  
+
